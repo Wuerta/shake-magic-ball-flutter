d17f6b9fa638288374cbec03c8e41e6bfed7bd70
Gabriel Wuerta <gabrielwuerta@gmail.com>
2021-08-29 12:11:02 -0300

    :speech_balloon: Update hint text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -69,7 +69,7 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Ask Me Anything!'),
         actions: [
           const CustomIconButton(
-            tooltip: 'Shake your phone or tap on triangle!',
+            tooltip: 'Shake your phone or tap on magic ball!',
             icon: Icon(Icons.info_outline),
           ),
         ],
