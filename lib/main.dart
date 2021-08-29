6349303fa86c6b54bfa41cf3edd5a6704daaaf78
Gabriel Wuerta <gabrielwuerta@gmail.com>
2021-08-29 11:21:39 -0300

    :recycle: Refactor and add Home Screen on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shake_ball_magic/screens/home_screen.dart';
 
-void main() {
-  runApp(ShakeBallMagic());
-}
+void main() => runApp(const ShakeBallMagic());
 
 class ShakeBallMagic extends StatelessWidget {
   const ShakeBallMagic({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const HomeScreen(),
+    );
   }
 }
