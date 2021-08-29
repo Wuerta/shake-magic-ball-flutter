f91086d0bfda17ac57c045fe93c5f0e8aa93a624
Gabriel Wuerta <gabrielwuerta@gmail.com>
2021-08-29 12:12:01 -0300

    :sparkles: Create magic_ball.dart

---
+++ lib/components/magic_ball.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
