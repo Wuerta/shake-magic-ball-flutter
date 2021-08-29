43f1065daad8c8cb4cdb9f202e310cfadcd4efb5
Gabriel Wuerta <gabrielwuerta@gmail.com>
2021-08-29 11:10:52 -0300

    :tada: Start the Shake Magic Ball Project

---
+++ android/app/src/main/kotlin/dev/wuerta/shake_ball_magic/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.wuerta.shake_ball_magic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ShakeBallMagic());
+}
+
+class ShakeBallMagic extends StatelessWidget {
+  const ShakeBallMagic({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
